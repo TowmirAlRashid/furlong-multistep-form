fa08bb932c36ce8c8196922732bf9c470a4ffba4
TowmirAlRashid <alrashidtowmir420@gmail.com>
2022-09-19 13:54:06 +0600

    added site title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import { Box, Container, Paper, Typography } from "@mui/material";
 
 
 function App() {
   return (
     <div>
-      <h1>App</h1>
+      <Container 
+        component={Box} 
+        p={4}
+        sx={{
+          width: '60%',
+          margin: '20px auto'
+        }}
+      >
+        <Box
+          sx={{
+            width: '100%',
+            backgroundColor: 'black',
+            color: 'white',
+            textAlign: 'center',
+            p: '12px 15px'
+          }}
+        >
+          <Typography   
+            variant="h1"
+            sx={{ fontSize: '1.5rem' }}
+          >
+            Furlong Painting - Safe Work Method Statement (SWMS)
+          </Typography>
+        </Box>
+        <Paper component={Box} p={3}>
+          <h1>App</h1>
+        </Paper>
+      </Container>
     </div>
   );
 }
